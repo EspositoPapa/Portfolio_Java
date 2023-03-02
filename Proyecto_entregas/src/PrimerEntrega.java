cb8a298c5ebcc2881aff61e6a4a9caa35cbd3916
Esposito Micaela <102673065+EspositoPapa@users.noreply.github.com>
2023-03-02 17:39:16 -0300

    Primer ingreso de Java

---
+++ Proyecto_entregas/src/PrimerEntrega.java
@@ -0,0 +1,8 @@
+public class PrimerEntrega{
+        public static void main(String[] args) {
+            System.out.println("Tipos de datos Java: ");
+            System.out.println("Tipo entero:\n Byte\n short\n int\n long");
+            System.out.println("\nTipo flotante:\n float\n double");
+            System.out.println("\n char\n boolean\n string\n Interger");
+        }
+}
---
+++ Proyecto_entregas/src/SegundaEntrega.java
@@ -0,0 +1,9 @@
+public class SegundaEntrega {
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos, en segundos se ofrecerá un precio: ");
+        int precio= 110;
+        double IVA=0.21;
+
+        System.out.println("tu precio es: "+ precio*IVA);
+    }
+}
